a86818a67116ed758b361ad2a651bafac20d66a8
AtqiyaHaydar <ashford1524@gmail.com>
2024-11-23 15:39:45 +0700

    feat(schema): update schema and prisma

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -1,33 +1,32 @@
 import z from 'zod'
 
-export const signInSchema = z.object({
-  username: z.string().min(1),
-  password: z.string().min(1)
-})
 
+// Sign In Schema
+export const signInSchema = z.object({
+  username: z.string().nonempty(),
+  password: z.string().min(6).max(20), 
+});
 export type signInSchemaType = z.infer<typeof signInSchema>
 
+// Announcement Schema
 export const announcementSchema = z.object({
-  title: z.string().min(1),
-  content: z.string().min(1)
+  title: z.string().nonempty(),
+  content: z.string().min(8)
 })
-
 export type announcementSchemaType = z.infer<typeof announcementSchema>
-
 export type announcementSchemaTypeWithId = {
   id: string
   title: string
   content: string
 }
 
+// Handbook Schema
 export const handbookSchema = z.object({
-  day: z.string().min(1),
-  title: z.string().min(1),
-  link: z.string().min(1)
-})
-
+  day: z.string().nonempty(),
+  title: z.string().nonempty(), 
+  link: z.string().url().nonempty(),
+});
 export type handbookSchemaType = z.infer<typeof handbookSchema>
-
 export type handbookSchemaTypeWithId = {
   id: string
   day: string
@@ -35,13 +34,11 @@ export type handbookSchemaTypeWithId = {
   link: string
 }
 
-// Schema untuk StudentAssignment
+// Student Assignment Schema
 export const studentAssignmentSchema = z.object({
-  link: z.string().min(5),
+  link: z.string().nonempty(),
 });
-
 export type studentAssignmentType = z.infer<typeof studentAssignmentSchema>;
-
 export type studentAssignmentTypeWithId = {
   id: string;
   link: string;
@@ -50,17 +47,15 @@ export type studentAssignmentTypeWithId = {
   assignmentId: string;
 };
 
-// Schema untuk AssignmentForStudent
+// Assignment For Student Schema
 export const assignmentForStudentSchema = z.object({
-  day: z.string().min(1),
-  title: z.string().min(1),
-  description: z.string().min(1),
-  dueDate: z.string(),
-  linkAttach: z.string()
+  day: z.string().nonempty(),
+  title: z.string().nonempty(), 
+  description: z.string().nonempty(), 
+  dueDate: z.string().nonempty(),
+  linkAttach: z.string().url().optional(),
 });
-
 export type assignmentForStudentType = z.infer<typeof assignmentForStudentSchema>;
-
 export type assignmentForStudentTypeWithId = {
   id: string;
   day: string;
